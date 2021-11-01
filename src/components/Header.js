c302b3fb1764f040812f3afbfefcadf7a3eddaba
Dmitry <elvs18@mail.ru>
2021-11-01 16:55:35 +0100

    First push

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button
+                color={showAdd ? 'black' : 'red'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd}
+            />
+        </header>
+    )
+}
+
+// Header.defaultProps = {
+//     title: 'Task Tracker',
+// }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// const headingStuling = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header
